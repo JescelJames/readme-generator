cfa2751d210f8c83468b6525942a0e89f5c75f92
Jescel James <jesceljames@gmail.com>
2023-12-17 11:37:49 -0500

    exports renderLicense in index.js

--- index.js
+++ index.js
@@ -4,8 +4,7 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 
-const generateMarkdown = require('./generateMarkdown');
-
+const {renderLicenseBadge, generateMarkdown} = require('./generateMarkdown');
 
 
 
